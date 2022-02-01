3421bce663c609ebdec7fbb972d0e94b27784e5e
Juhani <juhanirusi@yahoo.com>
2022-02-01 14:57:40 +0200

    Added more content.

--- input_messages.py
+++ input_messages.py
@@ -1,12 +1,45 @@
-def wrong_play_game_input(wrong_play_game_inputs):
+def wrong_game_input(wrong_game_inputs):
     """
     If the user inputs something else than 'yes' or 'no' to the play_game input these messages are shown
     """
-    if wrong_play_game_inputs == 1:
+    if wrong_game_inputs == 1:
         print("That's a wrong input, I can't forward this to the computer. perhaps you made a typo? \U0001F92D Try again...")
-    elif wrong_play_game_inputs == 2:
+    elif wrong_game_inputs == 2:
         print("Well, now your playing games with me. \U0001F612 Try again...")
-    elif wrong_play_game_inputs == 3:
+    elif wrong_game_inputs == 3:
         print("Ok now I'm getting annoyed, \U0001F624 mess with me again, and I'll end the game....")
     else:
         pass
+
+
+def does_user_want_to_play(play_game):
+    """
+    When the user inputs 'no' i.e. doesn't want to play, this message is shown...
+    """
+    if play_game == "no":
+        print("Oops, now you hurt the computer's feelings and it decided to leave to count zeros and ones.")
+        print("However, the computer left one last message to you, but apparently it got so mad that it didn't bother to translate it completely...\n")
+        print("You are such a 01110000 01100001 01110010 01110100 01111001 00100000 01110000 01101111 01101111 01110000 01100101 01110010, and I never wanna play with you again!")
+        print("...I have no idea what that means, \U0001F914 but the computer isn't happy. \U0001F620 \n")
+        
+
+def user_guessed_number(user_guess):
+    if user_guess > 100:
+        print("Well now you're going WAY over the roof. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100: ")
+    elif user_guess < 1:
+        print("Your guess is WAY too low. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100: ")
+    else:
+        pass
+    
+# THIS...
+def check_user_input(user_guess):
+    
+    try:
+        user_guess = int(user_guess)
+    except ValueError:
+        try:
+            user_guess = float(user_guess)
+        except ValueError:
+            user_guess = str(user_guess)
+    finally:
+        return user_guess
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
-import user_hints, input_messages
+import user_hints
+import input_messages
+import set_difficulty
 
 EASY_LEVEL_TURNS = 9
 MEDIUM_LEVEL_TURNS = 6
@@ -11,72 +13,78 @@ answer = random.randint(1, 100)
 
 print("\nThe computer generated this number:", answer, "\n")
 
-play_game = str(input("Do you want to play a game? 'yes' or 'no': ")).lower()
+# play_game = str(input("Do you want to play a game? 'yes' or 'no': ")).lower()
 # MOVE ALL THAT'S ABOVE TO THE BEGINNING OF THE game() FUNCTION WHEN EVERYTHING IS DONE!
 
-wrong_input = True
-while wrong_input:
-    if play_game == "no":
-        wrong_input = False
-        play_game = str(
-            input("Are you sure? The computer REALLY wants to play with you: ")).lower()
-        if play_game == "no":
-            print("""Oops, now you hurt the computer's feelings and it decided to leave to count zeros and ones.
-                    However, the computer left one last message to you, but apparently it got so mad that it didn't bother to translate it completely...\n
-                    "You are such a 01110000 01100001 01110010 01110100 01111001 00100000 01110000 01101111 01101111 01110000 01100101 01110010, and I don't never wanna play with you again!"
-                    ...I have no idea what that means, \U0001F914 but the computer isn't happy. \U0001F620 \n""")
-    elif play_game == "yes":
-        wrong_input = False
-        too_high_low_input = True
-        user_guess = int(input("Ok, the computer challenges you to guess a number between 1 and 100: "))
-        while too_high_low_input:
-            if user_guess > 100:
-                user_guess = int(input("Well now you're going WAY over the roof. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100: "))
-            elif user_guess < 1:
-                user_guess = int(input("Well now you're just messing with me. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100: "))
-            elif user_guess == float:
-                user_guess = int(input("FLOAT: "))
-            else:
-                too_high_low_input = False
-    elif play_game != 'no' or 'yes':
-        play_game = str(input("Sorry, I can't deliver that message to the computer \U0001F615 , so to (or not to) play the game, simply type 'yes' or 'no': ")).lower()
 
-if play_game == 'yes':
-    while user_guess != answer:
-        if guesses < 2:
-            user_hints.user_hint_1(answer, user_guess)
-        elif guesses < 3:
-            user_hints.user_hint_2(answer, user_guess)
+# FROM HERE--------------------------------------------------------------------
+# wrong_input = True
+# while wrong_input:
+#     if play_game == "no":
+#         wrong_input = False
+#         play_game = str(input("Are you sure? The computer REALLY wants to play with you: ")).lower()
+#         if play_game == "no":
+#             print("""Oops, now you hurt the computer's feelings and it decided to leave to count zeros and ones.
+#                     However, the computer left one last message to you, but apparently it got so mad that it didn't bother to translate it completely...\n
+#                     "You are such a 01110000 01100001 01110010 01110100 01111001 00100000 01110000 01101111 01101111 01110000 01100101 01110010, and I don't never wanna play with you again!"
+#                     ...I have no idea what that means, \U0001F914 but the computer isn't happy. \U0001F620 \n""")
+#     elif play_game == "yes":
+#         wrong_input = False
+#         too_high_low_input = True
+#         user_guess = int(input("Ok, the computer challenges you to guess a number between 1 and 100: "))
+#         while too_high_low_input:
+#             if user_guess > 100:
+#                 user_guess = int(input("Well now you're going WAY over the roof. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100: "))
+#             elif user_guess < 1:
+#                 user_guess = int(input("Well now you're just messing with me. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100: "))
+#             elif user_guess == float:
+#                 user_guess = int(input("FLOAT: "))
+#             else:
+#                 too_high_low_input = False
+#     elif play_game != 'no' or 'yes':
+#         play_game = str(input("Sorry, I can't deliver that message to the computer \U0001F615 , so to (or not to) play the game, simply type 'yes' or 'no': ")).lower()
 
-        user_guess = int(input("Guess again: "))
-        guesses += 1
+# if play_game == 'yes':
+#     while user_guess != answer:
+#         if guesses < 2:
+#             user_hints.user_hint_1(answer, user_guess)
+#         elif guesses < 3:
+#             user_hints.user_hint_2(answer, user_guess)
 
-    else:
-        play_game = False
-        print(f"\nEXCELLENT! You guessed the number right, it was {answer}. The computer praises you! \U0001F64C")
-        if guesses == 1:
-            print(f"You got the number right with your first guess. THAT'S ASTONISHING, even the computer is amazed \U0001F632 \n")
-        else:
-            print(f"You got the number right with {guesses} guesses. GOOD JOB!\n")
-        print("The computer had one last message to you, but I wasn't able to translate it completely...")
-        print('"You are such a 01100110 01110101 01101110 person!" ...Whatever that means? \U0001F914\n')
+#         user_guess = int(input("Guess again: "))
+#         guesses += 1
+
+#     else:
+#         play_game = False
+#         print(f"\nEXCELLENT! You guessed the number right, it was {answer}. The computer praises you! \U0001F64C")
+#         if guesses == 1:
+#             print(f"You got the number right with your first guess. THAT'S ASTONISHING, even the computer is amazed \U0001F632 \n")
+#         else:
+#             print(f"You got the number right with {guesses} guesses. GOOD JOB!\n")
+#         print("The computer had one last message to you, but I wasn't able to translate it completely...")
+#         print('"You are such a 01100110 01110101 01101110 person!" ...Whatever that means? \U0001F914\n')
+# TO HERE--------------------------------------------------------------------
+            
+            
 
 
 def game():
-    
-    
-    play_game = input("Do you want to play a game? 'yes' or 'no': ").lower()
-    wrong_play_game_inputs = 0
+
+    play_game = str(input("Do you want to play a game? 'yes' or 'no': ")).lower()
+    wrong_game_inputs = 0
     user_plays_game = True
+    first_no_input = True
+    user_guess_type = ""
+    game_on = True
     
-    
+
     def restart():
         """
-        The function for restarting the program.
+        The function for restarting the program (if user wants to).
         """
-        
+
         global user_plays_game
-        
+
         restart = input("Would you like to restart the game, and erase the computer's memory? ")
         if restart == "yes":
             game()
@@ -86,17 +94,42 @@ def game():
         else:
             user_plays_game = False
             print("Your input was confusing. Therefore I'll end the whole program.")
-        
+            
 
     while user_plays_game:
-        if play_game == "yes":
-            print("Let's play")
+        if play_game == 'yes':
+            user_guess = input("Ok, the computer challenges you to guess a number between 1 and 100: ")
+            
+            # FROM HERE...
+            input_messages.check_user_input(user_guess)
+            
+            if type(input_messages.check_user_input(user_guess)) == int:
+                user_guess = int(user_guess)
+                while game_on:
+                    input_messages.user_guessed_number(user_guess)
+                    user_guess = int(input("Guess again: "))
+            elif type(input_messages.check_user_input(user_guess)) == float:
+                print("seems to work with float")
+            elif type(input_messages.check_user_input(user_guess)) == str:
+                print("seems to work with str")
+            # ...TO HERE
+        
         elif play_game == 'no':
-            print("Don't play")
+            if first_no_input:
+                play_game = str(input("Are you sure? The computer REALLY wants to play with you: ")).lower()
+                first_no_input = False
+            input_messages.does_user_want_to_play(play_game)
+            if play_game == 'no':
+                print("GAME OVER")
+                restart()
+                break
+            else:
+                wrong_game_inputs = 0
+                continue
         else:
-            wrong_play_game_inputs += 1
-            input_messages.wrong_play_game_input(wrong_play_game_inputs)
-            if wrong_play_game_inputs == 4:
+            wrong_game_inputs += 1
+            input_messages.wrong_game_input(wrong_game_inputs)
+            if wrong_game_inputs == 4:
                 print("GAME OVER")
                 restart()
                 break
@@ -105,28 +138,3 @@ def game():
 
 
 game()
-
-# TRY THIS WITH FLOAT, INT, STRING...
-
-# def check_user_input(input):
-#     try:
-#         # Convert it into integer
-#         val = int(input)
-#         print("Input is an integer number. Number = ", val)
-#     except ValueError:
-#         try:
-#             # Convert it into float
-#             val = float(input)
-#             print("Input is a float  number. Number = ", val)
-#         except ValueError:
-#             print("No.. input is not a number. It's a string")
-
-
-# input1 = input("Enter your Age ")
-# check_user_input(input1)
-
-# input2 = input("Enter any number ")
-# check_user_input(input2)
-
-# input2 = input("Enter the last number ")
-# check_user_input(input2)
