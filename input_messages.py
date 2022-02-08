b5632060ba156fde88d6b611436a87a430f78291
Juhani <juhanirusi@yahoo.com>
2022-02-08 14:47:06 +0200

    "redecorated" code and removed useless code.

--- input_messages.py
+++ input_messages.py
@@ -14,33 +14,37 @@ def wrong_game_input(wrong_game_inputs):
 
 def does_user_want_to_play(play_game):
     """
-    When the user inputs 'no' i.e. doesn't want to play, this message is shown...
+    When the user inputs 'no' for the last time i.e. doesn't want to play, this message is shown...
     """
+    
     if play_game == "no":
         print("Oops, now you hurt the computer's feelings and it decided to leave to count zeros and ones.")
         print("However, the computer left one last message to you, but apparently it got so mad that it didn't bother to translate it completely...\n")
         print("You are such a 01110000 01100001 01110010 01110100 01111001 00100000 01110000 01101111 01101111 01110000 01100101 01110010, and I never wanna play with you again!")
         print("...I have no idea what that means, \U0001F914 but the computer isn't happy. \U0001F620 \n")
+    else:
+        pass
         
 
 def user_guessed_number(user_guess):
     if user_guess > 100:
-        print("Well now you're going WAY over the roof. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100: ")
+        print("Well now you're going WAY over the roof. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100!: ")
     elif user_guess < 1:
-        print("Your guess is WAY too low. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100: ")
+        print("Your guess is WAY too low. I can't take this to the computer when it explicitly said NUMBER BETWEEN 1 AND 100!: ")
     else:
         pass
     
-# THIS...
-def check_user_input(user_guess):
+    
+def check_user_input(guess):
+    
+    user_guess = input(guess)
     
     try:
-        user_guess = int(user_guess)
+        result = int(user_guess)
     except ValueError:
         try:
-            user_guess = float(user_guess)
+            result = float(user_guess)
         except ValueError:
-            user_guess = str(user_guess)
+            result = str(user_guess)
     finally:
-        print("Type inside function", type(user_guess))
-        return user_guess
+        return result
