b36a7ee3f4567ca907af8ccb9a4149897a56317c
Juhani <juhanirusi@yahoo.com>
2022-02-10 14:57:20 +0200

    Edited old hints & added one new hint.

--- user_hints.py
+++ user_hints.py
@@ -14,9 +14,9 @@ def user_hint_1(answer, user_guess):
             to_number = num_range[1]
 
     if user_guess in range(from_number, to_number):
-        print(f"Not quite. But you're pretty close because the number is between {from_number} and {to_number}.")
+        print(f"\nNot quite. But you're right, the number is between {from_number} and {to_number}.")
     else:
-        print(f"Nope. Let me give you the first hint, the number is between {from_number} and {to_number}.")
+        print(f"\nNope. Let me give you the first hint, the number is between {from_number} and {to_number}.")
         
 # Function for the second user hint --> user_hints.user_hint_2(number, user_guess)
 def user_hint_2(answer, user_guess):
@@ -29,13 +29,16 @@ def user_hint_2(answer, user_guess):
     is_even_number = answer % 2
     if is_even_number == 0:
         if user_guess % 2 == 0:
-            print("That's not the right number. But you're right, the number IS an even number.")
+            print("\nThat's not the right number. But you're right, the number IS an even number.")
         else:
-            print("Nope. My next tip is that the number IS an even number.")
+            print("\nNope. My next tip is that the number IS an even number.")
     else:
         if user_guess % 2 == 0:
-            print("Nope. My next tip is that the number ISN'T an even number.")
+            print("\nNope. My next tip is that the number ISN'T an even number.")
         else:
-            print("That's not the right number. But you're right, the number ISN'T an even number.")
+            print("\nThat's not the right number. But you're right, the number ISN'T an even number.")
 
-# HINT THAT TELLS THE USER WHAT ONE NUMBER (INT) THERE IS IN THE NUMBER
+# HINT THAT TELLS THE USER WHAT ONE NUMBER (INT) THERE IS IN THE NUMBER IF ANSWER LARGER THAN 9
+def user_hint_3(answer):
+    print(f"\nOk, this is my last tip, the last number of the answer is {answer % 10}.")
+    print("I bet that you're now able to figure it out. \U0001F609")
