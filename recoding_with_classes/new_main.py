1aff5292efdbbbcb47060ee78c4a87b99aa0ccb2
Juhani <juhanirusi@yahoo.com>
2022-03-07 15:01:56 +0200

    Recoding the game again.

--- recoding_with_classes/new_main.py
+++ recoding_with_classes/new_main.py
@@ -1,9 +1,31 @@
+from telnetlib import GA
 from check_input_type import CheckInput
 
-is_user_input_valid = CheckInput()
+user_input_valid = CheckInput()
 
-guess = input("Make a guess: ")
-if is_user_input_valid.check_user_input(guess):
-    print("Input value: VALID")
-else:
-    print("Input value: NOT VALID")
+import random
+
+class GameManager():
+    def __init__(self):
+        self.play = None
+        self.answer = random.randint(1, 100)
+
+    
+    def play_game(self):
+        self.play = str(input("Do you want to play a game? 'yes' or 'no': ")).lower()
+        if self.play in ('y', 'yup', 'yes'):
+            pass
+        else:
+            pass
+
+
+    def stuff(self):
+        guess = input("Make a guess: ")
+        if user_input_valid.check_user_input(guess):
+            print("Input value: VALID")
+        else:
+            print("Input value: NOT VALID")
+
+
+if __name__ == "__main__":
+    GameManager()
