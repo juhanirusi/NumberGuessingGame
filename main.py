17de49aab2f345889db2598864dd33d009d7ebe7
Juhani <juhanirusi@yahoo.com>
2022-03-28 14:48:07 +0300

    Renamed files to the versions coded with classes.

--- main.py
+++ main.py
@@ -1,122 +1,73 @@
+from check_input_type import CheckInput
+from difficulty import Set_Difficulty
+from messages import InputMessages
+
+user_input_valid = CheckInput()
+set_difficulty = Set_Difficulty()
+
 import random
-import user_hints
-import difficulty
-import input_messages
 
-def game(play_game):
-    """
-    This is the function for the main functionality of our game.
-    """
+class GameManager():
+    def __init__(self):
+        self.answer = 0
+        self.guesses_left = None
+        self.game_on = True
 
-    answer = random.randint(1, 100)
 
-    # TODO DELETE AFTER FINISHED PROJECT!
-    print("\nThe computer generated this number:", answer, "\n")
+    def play_game(self, play_game):
+        self.input_message = InputMessages()
 
-    # Just a set of numbers that have specific hints on them.
-    # Check user_hints.py --> famous_number_hint() to get the idea!
-    famous_numbers = {8, 99}
+        if play_game in ('y', 'yup', 'yes'):
+            self.answer = random.randint(1, 100)
+            print(self.answer) # TODO DELETE LATER!
+            self.guesses_left = set_difficulty.difficulty_level()
+            print("Ok, the computer challenges you to guess a number between 1 and 100...")
+            self.guess_number()
+        elif play_game in ('n', 'no', 'nope'):
+            print("Okay, I understand, see you next time.")
+        else:
+            print("Your input was confusing, I assume you don't want to play then.")
 
-    # Assign multiple variables to the same value
-    difficulty_not_set = user_plays_game = first_no_input = first_guess = True
-    wrong_game_inputs = user_hint = guesses = 0
+        self.restart()
 
-    def restart():
-        """
-        The function for restarting the program... if the user wants to.
-        """
 
-        global user_plays_game
+    def guess_number(self):
+        while self.game_on:
+            
+            self.input_message.guesses_left_message(self.guesses_left)
+            
+            self.guesses_left -= 1
+            guess = input("Make a guess: ")
+            print(self.guesses_left) # TODO DELETE LATER
 
+            if user_input_valid.check_user_input(guess) and self.guesses_left >= 0:
+                self.input_message.wait_time(self.guesses_left)
+                guess = int(guess)
+                if guess == self.answer:
+                    print(f"YES! YOU'RE RIGHT, THE ANSWER WAS: {self.answer}")
+                    break
+                elif guess != self.answer and self.guesses_left > 0:
+                    self.input_message.user_hint(self.answer, guess)
+
+            if self.guesses_left == 0:
+                print("Oh no, you've ran out of guesses and therefore, lost the game.")
+                print(f"The answer we were looking for was: {self.answer}")
+                print("Better luck next time!")
+                break
+
+
+    def restart(self):
         restart = input("Would you like to erase the computer's memory and restart the game? 'yes' or 'no': ")
         if restart in ('y', 'yup', 'yes'):
-            game(play_game)
+            self.game_on = True
+            self.play_game(play_game="yes")
         elif restart in ('n', 'no', 'nope'):
-            user_plays_game = False
-            print("Ok, I'll quit the program. Goodbye.")
+            print("Ok, I'll quit the program then. Goodbye.")
         else:
-            user_plays_game = False
             print("Your input was confusing. Therefore I'll end the whole program.")
 
 
-    while user_plays_game:
-        if play_game in ('y', 'yup', 'yes'):
-            if difficulty_not_set:
-                guesses_left = difficulty.difficulty_level("Choose a difficulty. Type 'easy', 'medium' or 'hard': ")
-                difficulty_not_set = False
-
-            if first_guess:
-                user_guess = input_messages.check_user_input("Ok, the computer challenges you to guess a number between 1 and 100: ")
-                first_guess = False
-            else:
-                if guesses_left == 1:
-                    print(f"You only have {guesses_left} attempt remaining. MAKE IT COUNT!")
-                elif guesses_left > 1:
-                    print(f"You still have {guesses_left} attempts remaining to guess the right number.")
-                user_guess = input_messages.check_user_input("\nGuess again: ")
-
-            guesses_left -= 1
-            guesses += 1
-
-            input_messages.wait_time(guesses, guesses_left)
-
-            if guesses_left <= 0 and user_guess != answer:
-                print("Oh no, your guess was wrong and you've ran out of guesses, so you lost the game. \U0001F61E")
-                restart()
-                break
-
-            if type(user_guess) == int:
-                input_messages.user_guessed_number(user_guess)
-                if user_guess != answer and user_guess < 100 and user_guess > 0:
-                    user_hint += 1
-                    if user_hint == 1:
-                        user_hints.user_hint_1(answer, user_guess)
-                    elif user_hint == 2:
-                        user_hints.user_hint_2(answer, user_guess)
-                        if answer in famous_numbers:
-                            user_hints.famous_number_hint(answer)
-                    elif user_hint >= 3:
-                        user_hints.user_hint_3(answer, user_guess)
-                elif user_guess == answer:
-                    print(f"CONGRATULATIONS! You guessed the number right, it was {answer}. The computer praises you! \U0001F64C")
-                    if guesses == 1:
-                        print(f"You got the number right with your first guess. THAT'S ASTONISHING, even the computer is amazed \U0001F632 \n")
-                    else:
-                        print(f"You got the number right with {guesses} guesses. GOOD JOB!\n")
-                    print("The computer had one last message to you, but I wasn't able to translate it completely...")
-                    print('"You are such a 01100110 01110101 01101110 person!" ...Whatever that means? \U0001F914\n')
-                    restart()
-                    break
-
-            else:
-                if type(user_guess) == float:
-                    print("You inputted a float number, it NEEDS TO BE AN INTEGER! Try again...")
-                elif type(user_guess) == str:
-                    print("You inputted a string, it NEEDS TO BE AN INTEGER! Try again...")
-
-        elif play_game in ('n', 'no', 'nope'):
-            if first_no_input:
-                play_game = str(input("Are you sure? The computer REALLY wants to play with you: ")).lower()
-                first_no_input = False
-            input_messages.does_user_want_to_play(play_game)
-            if play_game in ('n', 'no', 'nope'):
-                print("GAME OVER")
-                restart()
-                break
-            else:
-                wrong_game_inputs = 0
-                continue
-        else:
-            wrong_game_inputs += 1
-            input_messages.wrong_game_input(wrong_game_inputs)
-            if wrong_game_inputs == 4:
-                print("GAME OVER")
-                restart()
-                break
-            else:
-                play_game = input("Simply type 'yes' if you want to play and 'no' if you don't: ").lower()
-
-
 if __name__ == "__main__":
-    play_game = str(input("Do you want to play a game? 'yes' or 'no': ")).lower()
-    game(play_game)
+    game = GameManager()
+    play_game = str(input("Do you want to play? 'yes' or 'no': ")).lower()
+    game.play_game(play_game)
--- user_hints.py
+++ user_hints.py
@@ -1,60 +1,56 @@
-# Function for the second user hint --> user_hints.user_hint_1(number, user_guess)
-def user_hint_1(answer, user_guess):
-    """
-    Checks the number range of the computer generated number
-    from 'number_ranges' list and assigns it into the 'from_number' & 'to_number' variables.
+class UserHints:
+    def __init__(self):
+        self.number_ranges = [
+            (1, 10),
+            (11, 20),
+            (21, 30),
+            (31, 40),
+            (41, 50),
+            (51, 60),
+            (61, 70),
+            (71, 80),
+            (81, 90),
+            (91, 100)]
 
-    After that, based on user input, the function shows a proper message to help the user move forward in the game.
-    """
-    number_ranges = [(1, 10), (11, 20), (21, 30), (31, 40), (41, 50), (51, 60), (61, 70), (71, 80), (81, 90), (91, 100)]
 
-    for num_range in number_ranges:
-        if answer in range(num_range[0], (num_range[1] + 1)):
-            from_number = num_range[0]
-            to_number = num_range[1]
+    def user_hint_1(self, answer, guess):
+        for num_range in self.number_ranges:
+            if answer in range(num_range[0], (num_range[1] + 1)):
+                from_number = num_range[0]
+                to_number = num_range[1]
 
-    if user_guess in range(from_number, to_number):
-        print(f"Not quite. But you're right, the number is between {from_number} and {to_number}.")
-    else:
-        print(f"Nope. Let me give you a hint, the number is between {from_number} and {to_number}.")
-        
-
-# Function for the second user hint --> user_hints.user_hint_2(number, user_guess)
-def user_hint_2(answer, user_guess):
-    """
-    Checks whether the random number is an even or uneven number,
-    and is the user input an even or uneven number.
-
-    After that, based on user input, the function shows a proper message to help the user move forward in the game.
-    """
-    
-    if answer % 2 == 0:
-        if user_guess % 2 == 0:
-            print("That's not the right number. But you're right, the number IS an even number.")
+        if guess in range(from_number, to_number):
+            print(f"Not quite. But you're right, the number is between {from_number} and {to_number}.")
         else:
-            print("Nope. My next tip is that the number IS an even number.")
-    else:
-        if user_guess % 2 == 0:
-            print("Nope. My next tip is that the number ISN'T an even number.")
+            print(f"Nope. Let me give you a hint, the number is between {from_number} and {to_number}.")
+
+
+    def user_hint_2(self, answer, guess):
+        if answer % 2 == 0:
+            if guess % 2 == 0:
+                print("That's not the right number. But you're right, the number IS an even number.")
+            else:
+                print("Nope. Here's a tip, the number IS an even number.")
         else:
-            print("That's not the right number. But you're right, the number ISN'T an even number.")
+            if guess % 2 == 0:
+                print("Nope. Here's a tip, the number ISN'T an even number.")
+            else:
+                print("That's not the right number. But you're right, the number ISN'T an even number.")
 
 
-# HINT THAT TELLS THE USER IF THE THEIR GUESS IS HIGHER OR LOWER THAN THE CORRECT ANSWER
-def user_hint_3(answer, user_guess):
-    if user_guess < answer:
-        print("That's not the correct answer either. The correct answer is HIGHER than your guess.")
-    else:
-        print("That's not the correct answer either. The correct answer is LOWER than your guess.")
+    def user_hint_3(self, answer, guess):
+        if guess < answer:
+            print("That's not the correct answer either. The correct answer is HIGHER than your guess.")
+        else:
+            print("That's not the correct answer either. The correct answer is LOWER than your guess.")
 
 
-# A HINT THAT TELLS THE USER IF THE NUMBER IS FAMOUS FROM SOMEWHERE
-def famous_number_hint(answer):
-    print("\n\U0001F4A1EXTRA HINT!\U0001F4A1")
-    match answer:
-        case 99:
-            print("This number is also the jersey number of the most successful ice hockey player\n")
-        case 8:
-            print("Turning this number sideways will resemble the infinity symbol\n")
-        case _:
-            pass # This is done if answer is not found --> JUST IN CASE!
+    def famous_number_hint(self, answer):
+        print("\n\U0001F4A1EXTRA HINT!\U0001F4A1")
+        match answer:
+            case 99:
+                print("This number is also the jersey number of the most successful ice hockey player\n")
+            case 8:
+                print("Turning this number sideways will resemble the infinity symbol\n")
+            case _:
+                pass  # This is done if answer is not found --> JUST IN CASE!
